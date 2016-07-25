d1d78bb8115a9fc6bb04328a2121981af64c06ab
Lambdell <speedcell4@gmail.com>
2016-07-25 23:28:30 +0900

    force to rename

--- CompareNat/parser.py
+++ CompareNat/parser.py
@@ -1,5 +1,5 @@
 from CompareNat.rule import IsLessThan
-from Nat.parser import nat
+from Nat.miao import nat
 from bases.parser import string, spaces, pure
 
 assertion = pure(lambda a: lambda b: IsLessThan(a, b)) + nat + (spaces >> string(r'is less than') >> spaces >> nat)
